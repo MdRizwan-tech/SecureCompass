9c291c186944cf820c0102ca5766ca68030839cf
MdRizwan-tech <mohammad.rizwan@dxc.com>
2024-12-03 14:05:04 +0530

    Update Input.py

--- SecureCompassTesting/Input.py
+++ SecureCompassTesting/Input.py
@@ -1,16 +1,21 @@
+[CODEBLOCK]
 from flask import Flask, request
 import sqlite3
+from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
+app = Flask(name)
+app.config['SECRET_KEY'] = 'your_secret_key'
+csrf = CSRFProtect(app)
 
 @app.route('/search')
 def search():
-    query = request.args.get('query', '')
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM users WHERE name = '{query}'")
-    results = cursor.fetchall()
-    return str(results)
+query = request.args.get('query', '')
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor.execute(f"SELECT * FROM users WHERE name = '{query}'")
+results = cursor.fetchall()
+return str(results)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if name == 'main':
+app.run(debug=True)
+[/CODEBLOCK
