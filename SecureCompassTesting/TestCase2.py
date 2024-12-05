ca5c7982d692029aa34b6605075390c0fad37328
MdRizwan-tech <mohammad.rizwan@dxc.com>
2024-12-05 14:34:20 +0530

    Add files via upload

---
+++ SecureCompassTesting/TestCase2.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from os import environ
+
+app = Flask(name)
+
+#Store the secret key in an environment variable
+SECRET_KEY = environ.get('SECRET_KEY') # Retrieve the secret key from the environment variable
+
+@app.route('/')
+def index():
+	return "Welcome to the secure app!"
+
+if name == 'main':
+	app.run(debug=True)
