ed6ebd46cc924cb1f1b5ddd7c2b653da6e71e13a
MdRizwan-tech <mohammad.rizwan@dxc.com>
2024-12-05 15:00:07 +0530

    Update TestCaseJayaraj.java with Input code

--- SecureCompassTesting/TestCaseJayaraj.java
+++ SecureCompassTesting/TestCaseJayaraj.java
@@ -1,35 +1,40 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SQL Injection Vulnerability</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="SQLInjectionVulnerability" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username"><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.servlet.http.HttpServletRequest;
 
 public class SQLInjectionVulnerability {
-public void doPost(HttpServletRequest request) {
-String username = request.getParameter("username");
-String password = request.getParameter("password");
-
-    try {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "password");
-
-        // Prepared statement to prevent SQL injection
-        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
-        PreparedStatement stmt = conn.prepareStatement(query);
-        stmt.setString(1, username);
-        stmt.setString(2, password);
-
-        ResultSet rs = stmt.executeQuery();
-        while (rs.next()) {
-            // Process result
+    public void doPost(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "password");
+            Statement stmt = conn.createStatement();
+            
+            // Vulnerable query
+            String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+            stmt.executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        rs.close();
-        stmt.close();
-        conn.close();
-    } catch (Exception e) {
-        e.printStackTrace();
     }
-}
 }
