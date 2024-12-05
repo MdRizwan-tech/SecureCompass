2baa1178c3670ce81b4eac69fa71239b34eaaabc
MdRizwan-tech <mohammad.rizwan@dxc.com>
2024-12-05 14:19:14 +0530

    Update Input.py

--- SecureCompassTesting/Input.py
+++ SecureCompassTesting/Input.py
@@ -1,14 +1,19 @@
-from flask import Flask
-from os import environ
+from flask import Flask, request
+import sqlite3
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(name)
+app.config['SECRET_KEY'] = 'your_secret_key'
+csrf = CSRFProtect(app)
 
-Store the secret key in an environment variable
-SECRET_KEY = environ.get('SECRET_KEY') # Retrieve the secret key from the environment variable
-
-@app.route('/')
-def index():
-return "Welcome to the secure app!"
+@app.route('/search')
+def search():
+query = request.args.get('query', '')
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor.execute(f"SELECT * FROM users WHERE name = '{query}'")
+results = cursor.fetchall()
+return str(results)
 
 if name == 'main':
 app.run(debug=True)
